cd56e1ae5ef8a954316d015bed0b207bcb04127d
Leo <leonardorenepereira@gmail.com>
2021-01-04 08:28:31 -0300

    modal new demand its ok

--- src/components/Modals/NewDemand.js
+++ src/components/Modals/NewDemand.js
@@ -1,36 +1,66 @@
 import React from 'react'
-import { Modal, ModalBody, ModalHeader } from 'reactstrap'
-import Image from '../../assets/images/conquista.png'
+import { Modal, ModalBody } from 'reactstrap'
 import  '../../assets/css/bussiness.css'
  
 const Oportunity = (props) => {
     return (
-        <Modal isOpen={true}>
+        <Modal isOpen={true} >
             <ModalBody>
-                <div class="row">
-                    <div class="col-10">
-                        <h2 class="font-blue">Oportunidade conquistada Parabéns!!!!</h2>
+              <div class="row justify-content-md-center">
+                <div class="col-12 text-center mb-5">
+                  <h3 class="font-blue">Nova Demanda</h3>
+                </div>
+                <form action="create-service" method="post">
+                <div class="col-12">
+                  <div class="row">
+                  <div class="col-12">
+                      <div class="groupForm">
+                        <h5 class="card-subtitle mb-2 text-muted">Qual o tipo de oportunidade</h5>
+                        <select name="type_service" id="cars">
+                          <option value="demanda">Demanda</option>
+                          <option value="oferta">Oferta</option>
+                        </select>
+                      </div>
+                      <div class="groupForm">
+                        <h5 class="card-subtitle mb-2 text-muted">Qual o tipo de demanda</h5>
+                        <select name="type_demand" id="cars">
+                          <option value="produto">Produto</option>
+                          <option value="serviço">Serviço</option>
+                        </select>
+
+                      </div>
+                      <div class="groupForm">
+                        <h5 class="card-subtitle mb-2 text-muted">Título</h5>
+                        <input type="text" name="title" />
+                        <input type="text" name="owner_id" hidden value="1" />
+                      </div>
+                      <div class="groupForm">
+                        <h5 class="card-subtitle mb-2 text-muted">Descrição</h5>
+                        <textarea name="description" id="" cols="30" rows="5"></textarea>
+                      </div>
+                      <div class="groupForm">
+                        <h5 class="card-subtitle mb-2 text-muted">Segmento em atuação</h5>
+                        <select name="id_segment" id="cars">
+                          <option value="1">Produto</option>
+                          <option value="2">Serviço</option>
+                        </select>
+
+                      </div>
+                      <div class="groupForm">
+                        <h5 class="card-subtitle mb-2 text-muted">Serviços prestados</h5>
+                        <select name="id_service" id="cars">
+                          <option value="1">Produto</option>
+                          <option value="2">Serviço</option>
+                        </select>
+
+                      </div>
+
+                      <button type="submit">Cadastrar</button>
                     </div>
+                  </div>
                 </div>
-                <div class="row justify-content-center">
-              <div class="col-7" style={{textAlign:'center'}}>
-                <img  width="450px" src={Image} alt="" />
+                </form>
               </div>
-              <div class="col-7">
-                <div class="row">
-                  <h5 class="font-purple">Título</h5>
-                </div>
-                <div class="row">
-                  <p>ajnkjfansdfjkadsnfjkas</p>
-                </div>
-                <div class="row">
-                  <h5 class="font-purple">Valor da conquista</h5>
-                </div>
-                <div class="row">
-                  <p>Qual o valor total da conquista: R$ 3.000,00</p>
-                </div>    
-              </div>
-            </div>
             </ModalBody>
         </Modal>
     )
